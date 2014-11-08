ee2368b504a53534b5b01483b5280ccac95cd80f
Vaibhav <vaibhavparnalia@gmail.com>
2014-11-08 11:19:43 -0800

    nicer code flow

--- __main__.py
+++ __main__.py
@@ -1,20 +1,29 @@
 import urllib.request
 
-page = urllib.request.urlopen('http://www.imdb.com/showtimes/location')
-page_content = page.read()
+def readHTML(url):
+    page = urllib.request.urlopen(url)
+    return page.read()
 
-with open("html_cache/now_showing.html", "wb") as file:
-    file.write(page_content)
-file.close()
+def filterMovieID(file):
+    movieID = []
+    with open(file, "r", encoding="utf8") as file:
+        for line in iter(file):
+            if "/showtimes/title/" in line:
+                if "tt" in line:
+                    indexStart = line.index("tt")
+                    indexEnd = indexStart + line[indexStart:].index('/')
+                    if line[indexStart:indexEnd] not in movieID:
+                        movieID.append(line[indexStart:indexEnd])
+    return movieID
+    
+def getNowShowing():
+    page_content = readHTML("http://www.imdb.com/showtimes/location")
+    
+    with open("html_cache/now_showing.html", "wb") as file:
+        file.write(page_content)
+    file.close()
+    return filterMovieID("html_cache/now_showing.html")
 
-movieId = []
-with open("html_cache/now_showing.html", "r", encoding="utf8") as file:
-    for line in iter(file):
-        if "/showtimes/title/" in line:
-            if "tt" in line:
-                indexStart = line.index("tt")
-                indexEnd = indexStart + line[indexStart:].index('/')
-                if line[indexStart:indexEnd] not in movieId:
-                    movieId.append(line[indexStart:indexEnd])
-print(movieId)
-print(len(movieId))
+if __name__ == "__main__":
+    nowShowing = getNowShowing()
+    print(nowShowing)
